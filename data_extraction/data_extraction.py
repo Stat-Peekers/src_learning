988f6d7b811a7cd0937dbde778172344eecbb198
Sushant Rao <raosushant7@gmail.com>
2020-05-28 19:26:52 +0530

    Added separation of nested dicts in match data

--- data_extraction/data_extraction.py
+++ data_extraction/data_extraction.py
@@ -3,15 +3,18 @@
 import os
 import sys
 import json
-
 import pandas as pd
 
 from pandas.io.json import json_normalize
 
-from common_functions import get_data_from_file_json
+from common_files.common_functions import get_data_from_file_json
 
 # Switch Working Directory 1 step higher:
-parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir))
+src_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir))
+sys.path.append(src_dir)
+os.chdir(src_dir)
+
+parent_dir = os.path.abspath(os.path.join(src_dir, os.pardir))
 sys.path.append(parent_dir)
 os.chdir(parent_dir)
 
@@ -33,47 +36,94 @@ class DataExtraction:
         self.event_df = pd.DataFrame()
 
     # ******************* Get competition Data ******************* #
-    def get_comp_json(self):
+    def _get_comp_json(self):
         self.comp_json = json.load(open(self.data_dir + "competitions.json"))
 
-    def get_comp_df(self):
+    def _get_comp_df(self):
         self.comp_df = json_normalize(self.comp_json)
 
-    def get_comp_final_call(self):
-        self.get_comp_json()
-        self.get_comp_df()
+    def _get_comp_final_call(self):
+        self._get_comp_json()
+        self._get_comp_df()
 
-    def get_comp_info(self):
-        self.get_comp_final_call()
+    def _get_comp_info(self):
+        self._get_comp_final_call()
 
     # ********************** Get Event Data ********************** #
     def get_event_data(self, e_filename):
         self.event_json = get_data_from_file_json(self.event_data_dir + e_filename + ".json")
 
     # ********************** Get Match Data ********************** #
-    def get_match_json(self):
+    def _get_match_json(self):
         comp_folder_list = [folder_name for folder_name in os.listdir(self.match_data_dir)]
         self.match_jsons = [json.load(open(self.match_data_dir + c_folder + "/" + m_filename, encoding="utf-8"))
                             for c_folder in comp_folder_list for m_filename in os.listdir(self.match_data_dir + c_folder)]
 
-    def get_match_df(self):
+    def _get_match_df(self):
         for tour_index in range(len(self.match_jsons)):
             self.match_df = self.match_df.append(self.match_jsons[tour_index])
+        self.match_df.reset_index(drop=True, inplace=True)
 
-    def get_match_final_call(self):
-        self.get_match_json()
-        self.get_match_df()
+    def _get_comp_id(self):
+        self.match_df["competition_id"] = json_normalize(self.match_df["competition"])["competition_id"]
 
-    def get_match_info(self):
-        self.get_match_final_call()
+    def _get_comp_stage_id(self):
+        self.match_df["competition_stage_id"] = json_normalize(self.match_df["competition_stage"])["id"]
+
+    def _get_stadium_id(self):
+        try:
+            self.match_df["stadium_id"] = json_normalize(self.match_df["stadium"])["id"]
+        except AttributeError:
+            print("There are some null values in the Stadium column.\nReplacing them with empty dictionaries.")
+            self.match_df['stadium'] = self.match_df['stadium'].apply(lambda x: {} if pd.isna(x) else x)
+            self.match_df["stadium_id"] = json_normalize(self.match_df["stadium"])["id"]
+
+    def _get_referee_id(self):
+        try:
+            self.match_df["referee_id"] = json_normalize(self.match_df["referee"])["id"]
+        except AttributeError:
+            print("There are some null values in the Referee column.\nReplacing them with empty dictionaries.")
+            self.match_df['referee'] = self.match_df['referee'].apply(lambda x: {} if pd.isna(x) else x)
+            self.match_df["referee_id"] = json_normalize(self.match_df["referee"])["id"]
+
+    def _get_home_team_stats(self):
+        home_team_df = json_normalize(self.match_df["home_team"])
+        self.match_df = pd.concat([self.match_df, home_team_df], axis=1)
+        del home_team_df
+
+    def _get_away_team_stats(self):
+        away_team_df = json_normalize(self.match_df["away_team"])
+        self.match_df = pd.concat([self.match_df, away_team_df], axis=1)
+        del away_team_df
+
+    def _get_match_final_call(self):
+        self._get_match_json()
+        self._get_match_df()
+        self._get_comp_id()
+        self._get_comp_stage_id()
+        self._get_stadium_id()
+        self._get_home_team_stats()
+        self._get_away_team_stats()
+        self._get_referee_id()
+
+    def _get_match_info(self):
+        self._get_match_final_call()
+
+    def final_data_extraction_call(self):
+        # Extract competition info and arrange it for a DB table:
+        self._get_comp_info()
+
+        # Extract match info and arrange it for a DB table:
+        self._get_match_info()
+
+    def upload_data_to_db(self):
+        pass
 
 
 # testing:
 if __name__ == "__main__":
     ext_obj = DataExtraction()
-    # Get Comp Data:
-    ext_obj.get_comp_info()
+    # Get All Data:
+    ext_obj.final_data_extraction_call()
     ext_obj.comp_df.to_csv("data/input/comp_info.csv", index=False)
-    # Get Match info:
-    ext_obj.get_match_final_call()
     ext_obj.match_df.to_csv("data/input/match_info.csv", index=False)
